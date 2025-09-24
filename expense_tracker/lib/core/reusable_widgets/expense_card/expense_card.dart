0b7e271ee4a3638d080ad48ddc6995e7cd4e4982
iamKhaloyy <carlo.cabbo09@gmail.com>
2025-09-25 00:50:34 +0800

    Create expense_card.dart

---
+++ expense_tracker/lib/core/reusable_widgets/expense_card/expense_card.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+class ExpenseCard extends StatelessWidget {
+  final String description;
+  final double amount;
+  final DateTime date;
+  final String category;
+
+  const ExpenseCard({
+    super.key,
+    required this.description,
+    required this.amount,
+    required this.date,
+    required this.category,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+      elevation: 3,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          children: [
+            // Category Icon or Initial
+            CircleAvatar(
+              backgroundColor: Colors.black,
+              child: Icon(
+                category.toLowerCase() == 'bills'
+                    ? Icons.receipt_long
+                    : category.toLowerCase() == 'transportation'
+                    ? Icons.directions_car
+                    : category.toLowerCase() == 'food'
+                    ? Icons.fastfood
+                    : category.toLowerCase() == 'shopping'
+                    ? Icons.shopping_bag
+                    : Icons.category,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(width: 16),
+            // Details
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    description,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+
+                  Text(
+                    "${date.day}/${date.month}/${date.year}",
+                    style: const TextStyle(fontSize: 12, color: Colors.grey),
+                  ),
+                ],
+              ),
+            ),
+            // Amount
+            Text(
+              "₦${amount.toStringAsFixed(2)}",
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
